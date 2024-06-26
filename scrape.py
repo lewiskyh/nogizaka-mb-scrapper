c96790ddb03f7e1cb592883614841d302d347808
Lewis K <lewis.kwok.2023@bristol.ac.uk>
2024-06-26 16:43:46 +0100

    add gitignore

--- scrape.py
+++ scrape.py
@@ -1,6 +1,10 @@
 from json import JSONDecodeError
+
+import pymongo
 import requests
 import json
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 def fetch_json(url):
     try:
@@ -16,11 +20,24 @@ def fetch_json(url):
     except JSONDecodeError as e:
         print(f"(json decoding error: {e} )")
 
+
+def save_to_mongodb ():
+    from pymongo.mongo_client import MongoClient
+    from pymongo.server_api import ServerApi
+    uri = "mongodb+srv://lewiskwokyh:29YrIcfBnvKPo3yI@cluster0.xpzvbiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+
 def main():
-    URL = "https://www.nogizaka46.com/s/n46/api/list/member"
-    data = fetch_json(URL)
-    print(data)
+    ##URL = "https://www.nogizaka46.com/s/n46/api/list/member"
+    ##data = fetch_json(URL)
 
+    save_to_mongodb()
 
-if __name__ == '__main__':
-    main()
